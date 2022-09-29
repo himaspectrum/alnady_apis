8da90bbe9590d18c5e2b3dafbb71fb698d2bbf18
odoospectrum <odoo@sys-spectrum.com>
2022-09-29 16:23:58 +0200

    initial setup

---
+++ quickstart/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(APIView):
+    def get(self,request, *args, **kwargs):
+        return Response({'hello': 'world'})
+
