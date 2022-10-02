3fa3c0d25b273f8ba62420e9090532866a40229d
odoospectrum <odoo@sys-spectrum.com>
2022-10-02 09:50:31 +0200

    firt api call

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,9 +1,37 @@
+from pprint import pprint
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+import xmlrpc.client
+
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+url = 'http://localhost:8069'
+db = '6ou'
+username = 'admin@yahoo.com'
+password = env('DATABASE_PASS')
+
+
+
+
+
+
+# info = xmlrpc.client.ServerProxy('https://demo.odoo.com/start').start()
+# url, db, username, password = info['host'], info['database'], info['user'], info['password']
+# common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+common = xmlrpc.client.ServerProxy('%s/xmlrpc/2/common' % url)
+uid = common.authenticate(db, username, password, {})
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 class TestView(APIView):
     def get(self,request, *args, **kwargs):
-        return Response({'hello': 'world'})
+        result = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
+        pprint(result)
+        return Response({'result': result})
 
