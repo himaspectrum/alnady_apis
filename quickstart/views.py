e305850aca59a8c0cb2d8c6b8ba4e509b66b1414
odoospectrum <odoo@sys-spectrum.com>
2022-10-02 10:03:16 +0200

    remove unused lines

--- quickstart/views.py
+++ quickstart/views.py
@@ -16,15 +16,6 @@ url = 'http://localhost:8069'
 db = '6ou'
 username = 'admin@yahoo.com'
 password = env('DATABASE_PASS')
-
-
-
-
-
-
-# info = xmlrpc.client.ServerProxy('https://demo.odoo.com/start').start()
-# url, db, username, password = info['host'], info['database'], info['user'], info['password']
-# common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 common = xmlrpc.client.ServerProxy('%s/xmlrpc/2/common' % url)
 uid = common.authenticate(db, username, password, {})
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
@@ -32,6 +23,5 @@ models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 class TestView(APIView):
     def get(self,request, *args, **kwargs):
         result = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
-        pprint(result)
         return Response({'result': result})
 
