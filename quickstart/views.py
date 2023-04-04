2c476967b54d0f8f550c5393458066fb4709b7b5
odoospectrum <odoo@sys-spectrum.com>
2023-04-04 10:03:34 +0200

    get request for specific fields

--- quickstart/views.py
+++ quickstart/views.py
@@ -13,15 +13,18 @@ environ.Env.read_env()
 
 
 url = 'http://localhost:8069'
-db = '6ou'
-username = 'admin@yahoo.com'
-password = env('DATABASE_PASS')
+db = 'alnady'
+username = 'admin'
+password = 'admin'
+# password = env('DATABASE_PASS')
 common = xmlrpc.client.ServerProxy('%s/xmlrpc/2/common' % url)
 uid = common.authenticate(db, username, password, {})
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 class TestView(APIView):
     def get(self,request, *args, **kwargs):
-        result = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
+        result = models.execute_kw(db, uid, password, 'res.partner', 'search_read', 
+                   [], {'fields':["id",'name','email']})
+        # result = self.env["res.users"].search_read([("id", "!=", 0)], fields=["partner_id",'name','email'])
         return Response({'result': result})
 
