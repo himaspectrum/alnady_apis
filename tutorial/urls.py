8b29e3eadbf6abfc1f96bd81b5a156e07bc40d96
odoospectrum <odoo@sys-spectrum.com>
2023-04-18 13:12:18 +0200

    use gunicorn

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,6 +3,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path,include,re_path
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 schema_view = get_schema_view(
@@ -29,3 +31,6 @@ urlpatterns = [
   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
